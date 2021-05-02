bb9532a6b46c7f2cee2e35548653530550e05b25
Krish Kashiwala <69708017+KrishKashiwala@users.noreply.github.com>
2021-05-02 22:03:45 +0530

    Delete src/components directory

--- src/components/ChildComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ChildComponent({greetHandler}) {
-    return (
-        <div>
-            <button onClick = {() =>  greetHandler('child')}>Greet Parent!</button>
-        </div>
-    )
-}
