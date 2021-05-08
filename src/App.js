9f2b57341b4caa9f6447cb67921437e2d9f3358a
KrishKashiwala <krishkashiwala@gmail.com>
2021-05-08 15:46:44 +0530

    took a day to figure out how to fetch an api using useEffect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import React , {useEffect, useState}  from 'react'
-
-
-import { useForm } from './useForm';
-import useFetch from './useFetch'
+import React , {useState , useEffect} from 'react';
+// import UseFetch from './UseFetch'
+const API = (url) => {
+    const [data , setData]  = useState(null)
+    const [loading , setLoading] = useState(true)
+    useEffect(() =>{
+        async function onload(url){
+        const res = await fetch(url)
+        const data  =  await res.json();
+        const item  = data.info;
+        setData(item)
+        setLoading(false)
+        
+        }
+        onload(url);
+    }, [url] )
+    return {data , loading}
+}
 const App = () =>{
-    const[values , handleValues] = useForm({email : '' , password : ''})
-    const [count , setCounter] = useState(0);
-    const {data} = useFetch(`http://numbersapi.com/${count}/trivia`)
     
-    // useEffect (() =>{
-
-    //     const onMouseMove = e => {
-    //         console.log(e);
-    //     }
-    //     window.addEventListener('mousemove' , onMouseMove)
-    //     return () => {
-    //         window.removeEventListener("mousemove" , onMouseMove)
-    //     }
     
-    // } , []);
-    
-    return (
-        <>      
-            {/* <button onClick = {() => setToggleValue(!toggleValue)}>Toggle</button>
-            {toggleValue && <Hello/>} */}
-            
-            <h1>{!data ? "Loading..." : data}</h1>
-            <button onClick  = {() =>setCounter(count+1)}>Increment</button>
-            
-            <input type="text" name = "email" value = {values.email} onChange = { handleValues}/>
-            <input type="number" name = "password" value = {values.password} onChange = {handleValues}/>
-            <h1>{values.email} {values.password}</h1>
-            
-        </>
-    )    
+    const {data , loading} = API("https://api.randomuser.me/");
+    return(
+        <div>
+                    <button>Fetch Seed</button>
+                   
+                    {loading ? '...loading' : <div>{data.seed}</div>}
+        </div>
+    )
 }
 export default App;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,11 +1,26 @@
-import {useState , useEffect} from 'react'
- const useFetch = (url) => {
-    const [state , setState] = useState({data : null , loading : true})
+import React   , {useState , useEffect} from 'react';
+function UseFetch(url) {
+    const [count , setCount] = useState(0)
+    const [data , setData]  = useState(null)
+    const [loading , setLoading] = useState(true)
     useEffect(() =>{
-        setState(state => ({data : state.data , loading : true}));
-        fetch(url).then(res => res.text()).then(y => setState({data : y , loading : false}))
-    } , [url ,setState])
-
-    return state;
-}   
-export default useFetch;
+        document.title = `You clicked ${count} times`
+        async function onload(){
+        const res = await fetch("https://api.randomuser.me/")
+        const data  =  await res.json();
+        const item = data.info;
+        setData(item)
+        setLoading(false)
+        }
+        onload();
+    } ,[count, url] )
+  return (
+    <>
+            <button onClick = {() => setCount(count+1)}>Click {count} times</button>
+            
+            {loading ? '...loading' :<h1>{data.seed}</h1>}  
+            
+    </>
+  );
+}
+export default UseFetch;
