bf2c4147d4662dcb81134e11bb944e0d15f09969
krish kashiwala <krishkashiwala@gmail.com>
2021-06-10 15:16:16 +0530

    .

--- src/components/FormikExample.jsx
+++ src/components/FormikExample.jsx
@@ -4,7 +4,6 @@ import {
 	Field,
 	Form,
 	useField,
-	FieldAttributes,
 	FieldArray
 } from "formik";
 import {
@@ -16,6 +15,7 @@ import {
 	Select,
 	MenuItem
 } from "@material-ui/core";
+import * as yup from 'yup'
 const FormikExample = () => {
 	const MyRadio = ({ label, ...props }) => {
 		const [field] = useField(props)
@@ -24,22 +24,41 @@ const FormikExample = () => {
 		)
 
 	}
-	return (
-		<Formik initialValues={
-			{ name: '', email: '', password: '', checkME: false, cookies: [], dahi: '', pets: [{ type: "cat", name: "jarvis", id: "" + Math.random() }] }
+	const MyTextField = ({ placeholder, ...props }) => {
+		const [field, meta] = useField(props)
+		const errorText = meta.error && meta.touched ? meta.error : ''
+		return (
+			<TextField {...field} placeholder={placeholder} helperText={errorText} error={!!errorText} />
+		)
 
-		} onSubmit={(data, { setSubmitting }) => {
-			setSubmitting(true)
-			console.log(data)
-			setSubmitting(false)
-		}}>
+	}
+	const validationSchema = yup.object({
+		name: yup.string().required('Please Enter your name'),
+		email: yup.string().required('Please Enter your email'),
+		password: yup.string().required('Please Enter your password').matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+		),
+
+	});
+
+	return (
+		<Formik validateOnChange={true}
+			validationSchema={validationSchema}
+			initialValues={
+				{ name: '', email: '', password: '', checkME: false, cookies: [], dahi: '', pets: [{ type: "", name: "", id: "" + Math.random() }] }
+			}
+			onSubmit={(data, { setSubmitting }) => {
+				setSubmitting(true)
+				console.log(data)
+				setSubmitting(false)
+			}}
+		>
 
 			{({ values, isSubmitting }) => (
 
 				<Form>
-					<Field placeholder="first name" type="text" name="firstname" as={TextField} />
-					<Field placeholder="email" type="email" name="email" as={TextField} />
-					<Field placeholder="password" type="password" name="password" as={TextField} />
+					<MyTextField placeholder="first name" type="text" name="name" />
+					<MyTextField placeholder="email" type="email" name="email" />
+					<MyTextField placeholder="password" type="password" name="password" />
 					<Field type="checkbox" name="checkme" as={Checkbox} />
 					<div>cookies</div>
 					<Field type="checkbox" name="cookies" value="mam" as={Checkbox} />
@@ -49,8 +68,37 @@ const FormikExample = () => {
 					<MyRadio type="radio" name="dahi" value="berry" label="berry" />
 					<MyRadio type="radio" name="dahi" value="khata" label="khata" />
 					<MyRadio type="radio" name="dahi" value="simple" label="simple" />
+					<FieldArray name="pets">{arrayhelpers => {
+						return (
+							<div>
+								<Button onClick={() => (
+									arrayhelpers.push({
+										type: 'frog',
+										name: '',
+										id: "" + Math.random()
+									})
+								)}>Add pet</Button>
+								{values.pets.map((pet, index) => {
+
+									return (
+										<div key={pet.id}>
+											<MyTextField placeholder="pet name" name={`pets.${index}.name`} />
+											<Field name={`pets.${index}.type`}
+												type="select" as={Select}>
+												<MenuItem value="cat">Cat</MenuItem>
+												<MenuItem value="Dog">Dog</MenuItem>
+												<MenuItem value="Frog">Frog</MenuItem>
+											</Field>
+										</div>
+									)
+								})}
+							</div>
+
+						)
+					}}
+					</FieldArray>
 					<Button variant="contained" type="submit" disabled={isSubmitting}>submit</Button>
-					<pre>{JSON.stringify(values, null, 2)}</pre>
+					<pre>{JSON.stringify(values, null, 10)}</pre>
 				</Form>
 			)
 			}
